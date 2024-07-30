3026caa72c5bbe22b87d5dcbc0cc89560212ddf0
Cori <coribeecroft@gmail.com>
2024-07-30 14:31:10 -0700

    Finished the initial project setup and added "hello, world" test for react-native-skia.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,33 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import React from "react"
+import { Text, View } from "react-native"
+import { Canvas, Circle, Group } from "@shopify/react-native-skia"
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+export default function Index() {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Test />
+        </View>
+    )
+}
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
-  );
+const Test = () => {
+    const width = 256
+    const height = 256
+    const backgroundColor = "#555"
+    const r = width * 0.33
+    return (
+        <Canvas style={{ width, height, backgroundColor }}>
+            <Group blendMode="multiply">
+                <Circle cx={r} cy={r} r={r} color="cyan" />
+                <Circle cx={width - r} cy={r} r={r} color="magenta" />
+                <Circle cx={width / 2} cy={width - r} r={r} color="yellow" />
+            </Group>
+        </Canvas>
+    )
 }
